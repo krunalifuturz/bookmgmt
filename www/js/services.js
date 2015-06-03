ded7479515aee3e0a5f59be16586b96c99fca974
krunalifuturz <krunal.barot@ifuturz.com>
2015-06-03 19:54:39 +0530

    service temp

--- www/js/services.js
+++ www/js/services.js
@@ -4,9 +4,18 @@ angular.module('starter.services', [])
  * A simple example service that returns some data.
  * Need to required call api for fetching the book data. For temp basis I have create static data.
  */
-.factory('BookService', function() {
+.factory('BookService', function($http) {
   // Might use a resource here that returns a JSON array
 
+  $http.get('http://echo.jsontest.com/conditions/frightful').then(function(resp) {
+    //$scope.conditions = resp.data.conditions;
+	alert(resp.data.conditions);
+  }, function(err) {
+	  alert(err);
+    console.error('ERR', err);
+    // err.status will contain the status code
+  });
+  
   // dummy data
   var books = [
     { id: 0, bookname: 'How to teach people', authorname: 'Jennifer Schedule', publication: 'PHL' },
